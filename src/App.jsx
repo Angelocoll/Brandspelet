31bc5cbf0e7cbf850be850064e1f64900831fbed
Angelocoll <angelo.collocolo@yh.nackademin.se>
2025-03-01 13:30:25 +0100

    Lagt till mina senaste ändringar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BrandMap from "./Component/BrandMap";
+
+function App() {
+  return (
+    <div>
+      <BrandMap />
+    </div>
+  );
+}
+
+export default App;
